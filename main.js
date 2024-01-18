ff43b36bce79e24c06409b43fc6e2d4ce7e48412
Informatica <informatica@alumno.local>
2024-01-18 11:56:31 -0600

    Segundo commit

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+var teclado = document.getElementById("teclado");
 
+const buttons = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+buttons.forEach(function(texto){
+    var button = document.createElement("button");
+    button.textContent = texto;
+    teclado.appendChild(button)
+})
